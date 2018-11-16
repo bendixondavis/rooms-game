230cfa6cf2b081435307233df53fa2652dcc3458
Ben <bendixondavis@gmail.com>
2018-11-16 18:42:42 -0500

    added bin file to store description text of the story, got working so can parse out text based on header tags for each room section

--- rooms.py
+++ rooms.py
@@ -1,29 +1,16 @@
 import random
 
-# 0 in map represents haven't been to that room,
-# 1 repesents have been to it, X repesents current room
-map = [[0,0],[0,0]]
+def get_story_item(text,tag):
+    #this method is given a string and a tag, it searches for the tag
+    #and returns all text following that tag until it reachs a [
+    #this is meant to search text in a file that is in ini style where section headers are inside [] brackets
+    #but instead of key pairs what is in each section is just a block of text
 
-intro_text = ("You walk up to the old house at the end of the street. \n"
-    "The paint is chipped and falling off.  \nThe gutters are hanging "
-    "from the roof.  \nThe shingles are curled and falling off.  \n"
-    "Most of the windows are broken and filled in with cobwebs. \n"
-    "You decide to walk up to the door and knock.  \nThe door swings open "
-    "and you hear a low whispered voice say '{}, please come in'.  ")
-room1 = ("This room has a worn out, slumped bed.  "
-        "Next to the bed is a dresser made of old worn wood with a cracked mirror.")
-room2 = ("There is nothing of interest in this room.")
-room3 = ("This is a kitchen.  The stove is rusted and covered in old grease."
-        "There are pots and pans laying all over the floor.")
-room4 = ("This is a bathroom.  The toilet bowl is filled with brown slime "
-        "and the bathtub is stained and chipped.")
-house = [room1, room2, room3, room4]
-player = ""
+    index = text.find(tag) + len(tag) #finds the tag searched for and returns index of the first character past the tag
+    end_index = text.find("[" , index + len(tag)) #finds the index of where the first [ is after the tag
+    item = text[index:end_index] #gets the text after the tag
 
-def main():
-    player = get_player()
-    intro(player)
-    #pick_room()
+    return item
 
 def intro(player):
     print(intro_text.format(player))
@@ -32,13 +19,40 @@ def get_player():
     player = input("Hello, what is your name? ")
     return player
 
-def pick_room():
-    print(house[random.randint(1,4)])
+def pick_direction():
+    pick = input("Do you want to go Right(R), Left(L), Forward(F) or Backward(B)?")
+    return pick
 
 def check_map(map):
     print(map)
 
 def update_map(map,room):
+    print(map)
+
+def main():
+    player = get_player()
+    intro(player)
+    print(entry)
+    choice = pick_direction()
+    print(choice)
+
+# 0 in map represents haven't been to that room,
+# 1 repesents have been to it, X repesents current room
+map = [0,0,0]
+
+bin_file = open("rooms.bin","rb")   #opens bin file that contains all of the story text
+story = bin_file.read().decode()    #reads the contents of the file into a string
+
+#initialize all of the text descriptions for the game intro and all rooms
+intro_text = get_story_item(story,"[Intro]")
+entry = get_story_item(story,"[Entry]")
+closet = get_story_item(story,"[Closet]")
+bathroom = get_story_item(story,"[Bathroom]")
+kitchen = get_story_item(story,"[Kitchen]")
+bedroom = get_story_item(story,"[Bedroom]")
+house = [entry, closet, bathroom, kitchen, bedroom]
+
+player = ""
 
 if __name__=="__main__":
     main()
