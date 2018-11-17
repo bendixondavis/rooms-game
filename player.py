d272be2f8685ff50426d7705028b441e357c6277
Ben <bendixondavis@gmail.com>
2018-11-17 16:43:10 -0500

    added Player class in player.py added __init__.py to pull in class file

---
+++ player.py
@@ -0,0 +1,3 @@
+class Player:
+    def __init__(self,first_name):
+        self.first_name = first_name
--- rooms.py
+++ rooms.py
@@ -1,4 +1,4 @@
-import random
+from player import Player
 
 def get_story_item(text,tag):
     #this method is given a string and a tag, it searches for the tag
@@ -9,32 +9,32 @@ def get_story_item(text,tag):
     index = text.find(tag) + len(tag) #finds the tag searched for and returns index of the first character past the tag
     end_index = text.find("[" , index + len(tag)) #finds the index of where the first [ is after the tag
     item = text[index:end_index] #gets the text after the tag
-
     return item
 
 def intro(player):
     print(intro_text.format(player))
 
-def get_player():
-    player = input("Hello, what is your name? ")
-    return player
+def get_player_name():
+    player_name = input("Hello, what is your name? ")
+    return player_name
 
-def pick_direction():
-    pick = input("Do you want to go Right(R), Left(L), Forward(F) or Backward(B)?")
+def get_direction():
+    pick = input("Do you want to go Right(R), Left(L), Forward(F), Backward(B) or Run(R)?")
     return pick
 
-def check_map(map):
-    print(map)
-
-def update_map(map,room):
-    print(map)
+def get_rooms_connect(graph,key):
+    connections = graph.get(key)
+    return connections
 
 def main():
-    player = get_player()
-    intro(player)
-    print(entry)
-    choice = pick_direction()
-    print(choice)
+    player = Player(get_player_name())   #get input for player name
+    intro(player.first_name)   #format player name into intro text
+    print(entry_text)    #output entryway text
+    choice = get_direction()
+    while choice != "R":
+        print(choice)
+        choice = get_direction()
+        print(get_rooms_connect(house,"entry"))
 
 # 0 in map represents haven't been to that room,
 # 1 repesents have been to it, X repesents current room
@@ -45,14 +45,16 @@ story = bin_file.read().decode()    #reads the contents of the file into a strin
 
 #initialize all of the text descriptions for the game intro and all rooms
 intro_text = get_story_item(story,"[Intro]")
-entry = get_story_item(story,"[Entry]")
-closet = get_story_item(story,"[Closet]")
-bathroom = get_story_item(story,"[Bathroom]")
-kitchen = get_story_item(story,"[Kitchen]")
-bedroom = get_story_item(story,"[Bedroom]")
-house = [entry, closet, bathroom, kitchen, bedroom]
-
-player = ""
+entry_text = get_story_item(story,"[Entry]")
+closet_text = get_story_item(story,"[Closet]")
+bathroom_text = get_story_item(story,"[Bathroom]")
+kitchen_text = get_story_item(story,"[Kitchen]")
+bedroom_text = get_story_item(story,"[Bedroom]")
+house = {"entry":["closet","bedroom","kitchen"],
+        "closet":["entry"],
+        "bathroom":["bedroom"],
+        "kitchen":["entry"],
+        "bedroom":["entry","bathroom"]}
 
 if __name__=="__main__":
     main()
